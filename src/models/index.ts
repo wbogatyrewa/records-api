3c72ed34786d113a171150f52fb116e057cffd21
wbogatyrewa <veronika01-01@mail.ru>
2023-05-26 12:48:14 +0500

    connect to db

---
+++ src/models/index.ts
@@ -0,0 +1,21 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD
+} = process.env;
+
+const sequelize: Sequelize = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`, {dialect: "postgres"});
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.log(`Unable to connect to the database: ${error}`);
+});
+
