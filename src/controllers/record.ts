716097c9d369d1168385511dd59488cba39c69f3
wbogatyrewa <veronika01-01@mail.ru>
2023-05-27 00:21:12 +0500

    add get records with pagination

---
+++ src/controllers/record.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import RecordModel from '../models/record';
+
+interface Pagination {
+  limit: number;
+  offset: number;
+}
+
+interface PagingData {
+  totalItems: number;
+  records: RecordModel[];
+  totalPages: number;
+  currentPage: number;
+}
+
+const getPagination = (page: string, size: string): Pagination => {
+  const limit = size ? +size : 20;
+  const offset = page ? +page * limit : 0;
+  return { limit, offset };
+};
+
+const getPagingData = (data: any, page: string, limit: number): PagingData => {
+  const { count: totalItems, rows: records } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, records, totalPages, currentPage };
+};
+
+export const getRecords = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { page, size } = req.query;
+    const { limit, offset } = getPagination((page || '').toString(), (size || '').toString());
+    
+    const data = await RecordModel.findAndCountAll({ where: {}, limit, offset });
+    const records = getPagingData(data, (page || '').toString(), limit);
+    res.status(200).send(records);
+  } catch (error) {
+    res.status(500).send(`Getting records failed with error: ${error}`);
+  }
+};
+
+export const addRecord = async (req: Request, res: Response): Promise<void> => {
+  
+};
+
+export const editRecord = async (req: Request, res: Response): Promise<void> => {};
+
+export const deleteRecord = async (req: Request, res: Response): Promise<void> => {};
