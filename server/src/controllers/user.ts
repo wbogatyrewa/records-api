d622cfe6fb9b43ae6bee8de908de5fd47d5319e1
wbogatyrewa <veronika01-01@mail.ru>
2023-09-24 21:09:09 +0300

    changes

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,43 +1,45 @@
-import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import UserModel from '../models/user';
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import UserModel from "../models/user";
 
-/** 
- * Registers a new user with the given name and password. 
- * 
- * @async 
- * @method signup 
- * @param {Request} req - Express request object. 
- * @param {Response} res - Express response object. 
- * @returns {Promise<void>} - Promise that resolves when the user is registered. 
- * @throws {Error} - Throws an error if there is an issue with registration. 
- */ 
+/**
+ * Registers a new user with the given name and password.
+ *
+ * @async
+ * @method signup
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {Promise<void>} - Promise that resolves when the user is registered.
+ * @throws {Error} - Throws an error if there is an issue with registration.
+ */
 export const signup = async (req: Request, res: Response): Promise<void> => {
   try {
     const { name, password } = req.body;
     if (!name || !password) {
-      res.status(400).json({ message: 'Name and password are required' });
+      res.status(400).json({ message: "Name and password are required" });
       return;
     }
     const data = {
       name,
-      password: await bcrypt.hash(password, parseInt(process.env.SALT_OF_ROUNDS as string)),
+      password: await bcrypt.hash(
+        password,
+        parseInt(process.env.SALT_OF_ROUNDS as string)
+      ),
     };
     const user = await UserModel.create(data);
 
     if (user) {
-      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY || '', {
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY || "", {
         expiresIn: 1 * 24 * 60 * 60 * 1000,
       });
       res.status(201).send({
         token: token,
-        name: name
+        name: name,
       });
       return;
-    }
-    else {
-      res.status(409).send('Details are not correct');
+    } else {
+      res.status(409).send("Details are not correct");
       return;
     }
   } catch (error) {
@@ -45,45 +47,43 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-/** 
- * Logs in a user with the given name and password. 
- * 
- * @async 
- * @method login 
- * @param {Request} req - Express request object. 
- * @param {Response} res - Express response object. 
- * @returns {Promise<void>} - Promise that resolves when the user is logged in. 
- * @throws {Error} - Throws an error if there is an issue with logging in. 
- */ 
+/**
+ * Logs in a user with the given name and password.
+ *
+ * @async
+ * @method login
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @returns {Promise<void>} - Promise that resolves when the user is logged in.
+ * @throws {Error} - Throws an error if there is an issue with logging in.
+ */
 export const login = async (req: Request, res: Response): Promise<void> => {
   try {
-    const {name, password} = req.body;
+    const { name, password } = req.body;
     const user = await UserModel.findOne({
       where: {
-        name: name
-      }
+        name: name,
+      },
     });
     if (user) {
       const isSame = await bcrypt.compare(password, user.password);
 
       if (isSame) {
-        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY || '', {
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY || "", {
           expiresIn: 1 * 24 * 60 * 60 * 1000,
         });
 
         res.status(201).send({
           token: token,
-          name: name
+          name: name,
         });
         return;
-      }
-      else {
-        res.status(401).send('Authentication failed');
+      } else {
+        res.status(401).send("Authentication failed");
         return;
       }
-    }
-    else {
-      res.status(401).send('Authentication failed');
+    } else {
+      res.status(401).send("Authentication failed");
       return;
     }
   } catch (error) {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,35 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import sequelize from './models';
-import userRouter from './routes/user';
-import recordRouter from './routes/record';
-import multer from 'multer';
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import sequelize from "./models";
+import userRouter from "./routes/user";
+import recordRouter from "./routes/record";
+import multer from "multer";
 
 const app: Express = express();
 const port: Number = 5000;
 
 const options: cors.CorsOptions = {
   origin: `*`,
-  credentials: true
+  credentials: true,
 };
 
 const storageConfig = multer.diskStorage({
-  destination: (req, file, cb) =>{
-      cb(null, "media");
+  destination: (req, file, cb) => {
+    cb(null, "media");
   },
-  filename: (req, file, cb) =>{
-      cb(null, `${(new Date()).toISOString().replace(/:/g, '-')}.jpg`);
-  }
+  filename: (req, file, cb) => {
+    cb(null, `${new Date().toISOString().replace(/:/g, "-")}.jpg`);
+  },
 });
 
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/media', express.static('./media'));
+app.use("/media", express.static("./media"));
 app.use(express.static(__dirname));
-app.use(multer({storage:storageConfig}).single("media"));
+app.use(multer({ storage: storageConfig }).single("media"));
 
-app.use(function(req: Request, res: Response, next: NextFunction) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
   res.header(
     "Access-Control-Allow-Headers",
     "x-access-token, Origin, Content-Type, Accept"
@@ -38,11 +38,11 @@ app.use(function(req: Request, res: Response, next: NextFunction) {
 });
 
 sequelize.sync().then(() => {
-  console.log('Database has been synced');
+  console.log("Database has been synced");
 });
 
-app.use('/api/users', userRouter);
-app.use('/api/records', recordRouter);
+app.use("/api/users", userRouter);
+app.use("/api/records", recordRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
